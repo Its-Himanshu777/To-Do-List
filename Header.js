1e45ff5ade9a2ee250a8028e25d190313379c6c2
Himanshu Tripathi <90601437+Its-Himanshu777@users.noreply.github.com>
2024-09-08 09:39:34 +0530

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return <>
+    <nav>Get Ready To Complete Your Daily Goals</nav>
+    <div className="header">
+      <a href="/home">Home</a>
+      <a href="/about">About</a>
+      <a href="/contact">Contact</a>
+    </div>
+  </>;
+};
+export default Header;
